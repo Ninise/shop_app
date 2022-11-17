bcd527e1c898119a7a02f2e9f025d38150ac438a
nikita <nikitin.ninise@gmail.com>
2022-11-17 17:11:36 +0200

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Nikita on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
