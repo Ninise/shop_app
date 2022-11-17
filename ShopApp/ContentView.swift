8e06eacb48ea3ed4de53a477e893b998d41e0d99
nikita <nikitin.ninise@gmail.com>
2022-11-17 18:59:09 +0200

    initial commit

--- ShopApp/ContentView.swift
+++ ShopApp/ContentView.swift
@@ -8,19 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var search: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        ZStack {
+            
+            Color("Bg")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading) {
+                AppBarView()
+                
+                TagLineView()
+                    .padding()
+                
+                HStack {
+                    HStack {
+                        Image("Search")
+                            .padding(.trailing, 8)
+                        TextField("Search Furniture", text: $search)
+                    }
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .padding()
+                    
+                    Image("Scan")
+                        .padding()
+                        .background(Color("Primary"))
+                }
+               
+            }
         }
-        .padding()
+        
+
+        
+            
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Image("menu")
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10)
+            }.tint(Color(.white))
+            
+            Spacer()
+            
+            Image("Profile")
+                .resizable()
+                .frame(width: 42, height: 42)
+                .cornerRadius(10)
+            
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find the \nBest ")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("Primary"))
+        + Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Bold", size: 28))
+            .foregroundColor(Color("Primary"))
     }
 }
